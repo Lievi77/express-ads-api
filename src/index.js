b4f1b0fd2a56c381aeb3919a3f0576b162a6ca9b
Lev <lguzm038@uottawa.ca>
2022-05-20 09:15:47 -0500

    Implement mongo mock database

--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+const { startDatabase } = require('./database/mongo');
+const { insertAd, getAds } = require('./database/ads');
+const { deleteAd, updateAd } = require('./database/ads');
+
 // defining the Express app
 const app = express();
-
-// defining an array to work as the database 
-const ads = [
-    { title: 'Hello, world (again)!' }
-];
 
 // adding Helmet to enhance your API's security
 app.use(helmet());
@@ -26,8 +25,30 @@ app.use(cors());
 app.use(morgan('combined'));
 
 // defining an endpoint to return all ads
-app.get('/', (req, res) => {
-    res.send(ads);
+app.get('/', async (req, res) => {
+    res.send(await getAds());
+});
+
+// start the in-memory MongoDB instance
+startDatabase().then(async () => {
+    await insertAd({ title: 'Hello, now from the in-memory database!' })
+});
+
+app.post('/', async (req, res) => {
+    const newAd = req.body;
+    await insertAd(newAd);
+    res.send({ message: 'New ad inserted.' });
+});
+
+app.delete('/:id', async (req, res) => {
+    await deleteAd(req.params.id);
+    res.send({ message: 'Ad removed.' });
+});
+
+app.put('/:id', async (req, res) => {
+    const updatedAd = req.body;
+    await updateAd(req.params.id, updatedAd);
+    res.send({ message: 'Ad updated.' });
 });
 
 // starting the server
